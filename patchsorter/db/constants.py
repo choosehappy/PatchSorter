f1f2ac8b06c2d2a8e7cff60ad029afa6e790d93b
jackson <jjaco34@emory.edu>
2026-06-15 18:12:49 -0400

    feat: update Python version to 3.12, add Docker and Docker Compose configurations, and adjust environment variables

--- patchsorter/db/constants.py
+++ patchsorter/db/constants.py
@@ -1,13 +1,13 @@
 import os
 
 CITUS_HEAD_HOST = os.environ.get("CITUS_HEAD_HOST", "localhost")
-CITUS_HEAD_PORT = int(os.environ.get("CITUS_HEAD_PORT", "5432"))
+CITUS_HEAD_PORT = int(os.environ.get("CITUS_HEAD_PORT", "5439"))
 CITUS_HEAD_DB = os.environ.get("CITUS_HEAD_DB", "postgres")
 CITUS_HEAD_USER = os.environ.get("CITUS_HEAD_USER", "postgres")
 CITUS_HEAD_PASSWORD = os.environ.get("CITUS_HEAD_PASSWORD", "password")
 
 CITUS_WORKER_HOST = os.environ.get("CITUS_WORKER_HOST", "localhost")
-CITUS_WORKER_PORT = int(os.environ.get("CITUS_WORKER_PORT", "5432"))
+CITUS_WORKER_PORT = int(os.environ.get("CITUS_WORKER_PORT", "5439"))
 CITUS_WORKER_DB = os.environ.get("CITUS_WORKER_DB", "postgres")
 CITUS_WORKER_USER = os.environ.get("CITUS_WORKER_USER", "postgres")
 CITUS_WORKER_PASSWORD = os.environ.get("CITUS_WORKER_PASSWORD", "password")
